2040bbe2ba7aa7007822aadaf8207b2d102b583d
Marbew <lixundot@gmail.com>
2019-09-19 16:38:11 +0200

    add juma comment

--- helloworld.java
+++ helloworld.java
@@ -3,5 +3,6 @@ public class helloworld {
     public static void main(String args[]){
         System.out.println("I'm dancing again with the BB.king");
         System.out.println("Juma");
+        System.out.println("Empty");
     }
 }
