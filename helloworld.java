1e3d073400b773780a7c030717abc997c751b266
Marbew <lixundot@gmail.com>
2019-09-19 15:46:24 +0200

    removed a comment

--- helloworld.java
+++ helloworld.java
@@ -2,6 +2,5 @@
 public class helloworld {  
     public static void main(String args[]){
         System.out.println("I'm dancing again with the BB.king");
-        System.out.println("I'm dancing again with the BB.king");
     }
 }
